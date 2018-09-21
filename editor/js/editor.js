3bfb03d07645ab190e4873f976634c85331507b6
yucopowo <yucopowo@gmail.com>
2018-09-22 00:14:51 +0800

    sync scroll

--- editor/js/editor.js
+++ editor/js/editor.js
@@ -9,22 +9,59 @@
         lineWrapping: true
     });
 
-    function onScrolled() {
-        // console.log('$bus.panel', $bus.panel);
-        // if($bus.panel === 'editor'){
-        //     return;
-        // }
 
-        var scrollInfo = editor.getScrollInfo();
-        // console.log(scrollInfo);
-        var percentage = (
-            (scrollInfo.top / (scrollInfo.height - scrollInfo.clientHeight))
-        );
-        $bus.emit('editorScroll', percentage);
+    $bus.on('editorScroll', function (percentage) {
+        console.log('editorScroll');
+
+        if(!$bus.isSyncingPreviewScroll){
+
+            $bus.isSyncingPreviewScroll = true;
+
+            var scrollInfo = editor.getScrollInfo();
+            var top = percentage * (scrollInfo.height - scrollInfo.clientHeight);
+            console.log(top);
+            editor.scrollTo(0, top);
+        }
+
+        $bus.isSyncingEditorScroll = false;
+    });
+
+
+    // function onScrolled() {
+    //     // console.log('$bus.panel', $bus.panel);
+    //     // if($bus.panel === 'editor'){
+    //     //     return;
+    //     // }
+    //     // $bus.
+    //     // $bus.panel = 'editor';
+    //
+    //     var scrollInfo = editor.getScrollInfo();
+    //     // console.log(scrollInfo);
+    //     var percentage = (
+    //         (scrollInfo.top / (scrollInfo.height - scrollInfo.clientHeight))
+    //     );
+    //     $bus.emit('editorScroll', percentage);
+    // }
+
+    function onScrolled() {
+
+        if(!$bus.isSyncingPreviewScroll){
+
+            $bus.isSyncingPreviewScroll = true;
+
+            var scrollInfo = editor.getScrollInfo();
+            var percentage = (
+                (scrollInfo.top / (scrollInfo.height - scrollInfo.clientHeight))
+            );
+            $bus.emit('previewScroll', percentage);
+
+        }
+
+        $bus.isSyncingEditorScroll = false;
     }
     
-    editor.on("scroll", _.throttle(onScrolled, 100));
-    // editor.on("scroll", onScrolled);
+    // editor.on("scroll", _.throttle(onScrolled, 100));
+    editor.on("scroll", onScrolled);
 
     editor.on("change", function() {
         $bus.emit('preview', editor.getValue());
@@ -38,13 +75,20 @@
 
     });
 
-    $bus.on('previewScroll', function (percentage) {
+    // $bus.on('previewScroll', function (percentage) {
+    //
+    //
+    //
+    //     // console.log('previewScroll', percentage);
+    //     // editor.scrollTo(null, percentage * 762);
+    // });
 
-
-
-        // console.log('previewScroll', percentage);
-        editor.scrollTo(null, percentage * 762);
-    });
+    // console.log(editor);
+    // setTimeout(function () {
+    //     // var scrollInfo = editor.getScrollInfo();
+    //     // scrollInfo.
+    //     editor.scrollTo(0, 300);
+    // },  3000);
 
 
     $bus.emit('ready', 'editor');
--- preview/js/preview.js
+++ preview/js/preview.js
@@ -1,5 +1,7 @@
 (function ($bus) {
 
+    window.parent._preview = document;
+
     function getPercentage() {
         return window.scrollY / (document.body.clientHeight - $(window).height());
     }
@@ -9,37 +11,61 @@
         document.body.innerHTML = html;
     }
 
-    var scroll = new SmoothScroll();
+    // var scroll = new SmoothScroll();
+    // function onEditorScrolled(percentage) {
+    //     var top = percentage * document.body.clientHeight;
+    //     scroll.animateScroll(top, null, {
+    //         speed: 400,
+    //         easing: 'easeOutQuad'
+    //     });
+    // }
 
-    function onEditorScrolled(percentage) {
-        var top = percentage * document.body.clientHeight;
-        scroll.animateScroll(top, null, {
-            speed: 400,
-            easing: 'easeOutQuad'
-        });
+    function onPreviewScroll(percentage) {
+        var top = percentage * (document.body.clientHeight - document.documentElement.clientHeight);
+        document.documentElement.scrollTop = top;
     }
+
 
     $bus.on('preview', _.debounce(preview, 300, { 'maxWait': 1000 }));
 
     // $bus.on('editorScroll', _.throttle(onEditorScrolled, 300, {}));
-    $bus.on('editorScroll', onEditorScrolled);
+    // $bus.on('editorScroll', onEditorScrolled);
 
+    $bus.on('previewScroll', onPreviewScroll);
+
+
+    document.onscroll = function() {
+
+        if(!$bus.isSyncingEditorScroll){
+            $bus.isSyncingEditorScroll = true;
+
+            var percentage = document.documentElement.scrollTop / (document.body.clientHeight - document.documentElement.clientHeight);
+            console.log(percentage);
+            $bus.emit('editorScroll', percentage);
+        }
+
+        $bus.isSyncingPreviewScroll = false;
+
+
+    };
 
     // $('#preview').scroll(function () {
     //     console.log('#preview scroll');
     //     // $bus.emit('previewScroll');
     // });
 
-    $(window).scroll(function () {
-        if($bus.panel !== 'preview'){
-            return;
-        }
+    // $(window).scroll(function () {
+    //     if($bus.panel !== 'preview'){
+    //         return;
+    //     }
+    //
+    //     console.log('#preview scroll'+ new Date().getTime());
+    //
+    //     $bus.emit('previewScroll', getPercentage());
+    //
+    // });
 
-        console.log('#preview scroll'+ new Date().getTime());
 
-        $bus.emit('previewScroll', getPercentage());
-
-    });
 
     $bus.on('init', function () {
         console.log('preview init');
