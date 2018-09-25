f79abe4f681c53e88e66038aa70929c41b795b3c
yucopowo <yangxiuliang@moseeker.com>
2018-09-25 19:13:28 +0800

    add editor

---
+++ src/editor/editor.js
@@ -0,0 +1,16 @@
+const Event = require('../util/event');
+
+class Editor {
+
+    //get vis
+    getRange() {
+
+    }
+
+    scrollTo () {
+    }
+}
+
+Event.mixin(Editor);
+
+module.exports = Editor;
