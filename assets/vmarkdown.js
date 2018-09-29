b44a76ebb82d64b90119bc7f88f7d718518b81c1
yucopowo <yangxiuliang@moseeker.com>
2018-09-29 17:07:32 +0800

    support footnote

--- assets/vmarkdown.js
+++ assets/vmarkdown.js
@@ -271,10 +271,14 @@ const unified = __webpack_require__(1);
 const parse = __webpack_require__(16);
 const render = __webpack_require__(88);
 const paragraphHTML2Text = __webpack_require__(94);
+const footnote = __webpack_require__(95);
 
 module.exports = unified()
-    .use(parse, {})
+    .use(parse, {
+        footnotes: true
+    })
     .use(paragraphHTML2Text, {})
+    .use(footnote, {})
     .use(render, {})
     .freeze();
 
@@ -8468,6 +8472,177 @@ module.exports = function paragraphHTML(options = {}) {
     };
 
 };
+
+/***/ }),
+/* 95 */
+/***/ (function(module, exports, __webpack_require__) {
+
+var visit = __webpack_require__(40);
+var remove = __webpack_require__(96);
+
+module.exports = function checkbox(options = {}) {
+
+    return function transformer(root) {
+
+        visit(root, 'footnote', function (node) {
+            var children = node.children;
+            if(children.length>0) {
+                children[0].value = '[^'+children[0].value+']';
+            }
+        });
+
+        var index = 1;
+        var fndefs = [];
+        visit(root, 'footnoteReference', function (fnref) {
+            fnref.ref = 'fnref:'+fnref.identifier;
+            fnref.def = 'fndef:'+fnref.identifier;
+            fnref.value = index++;
+
+            var hasDef = false;
+
+            visit(root, {type: 'footnoteDefinition', identifier: fnref.identifier} , function (fndef) {
+
+                hasDef = true;
+
+                fndef.ref = 'fnref:'+fndef.identifier;
+                fndef.def = 'fndef:'+fndef.identifier;
+
+                visit(fndef, 'paragraph', function (node) {
+
+                    if( node.children.length === 0) {
+                        return;
+                    }
+
+                    var children = node.children;
+                    var position = children[0].position;
+
+                    var reverseNode = {
+                        url: '#' + fndef.ref,
+                        type: 'link',
+                        position: position,
+                        children:[
+                            {
+                                type: 'text',
+                                value: ' ↩',
+                                position: position
+                            }
+                        ]
+                    };
+
+                    node.children.push(reverseNode);
+
+                });
+
+                fndefs.push(fndef);
+
+            });
+
+            if(!hasDef){
+                fnref.type = 'text';
+                fnref.value = '[^'+fnref.identifier+']';
+            }
+        });
+
+        if( fndefs.length>0 ) {
+            remove(root, 'footnoteDefinition');
+
+            var children = [];
+            fndefs.forEach(function (fndef) {
+                children.push({
+                    type: 'listItem',
+                    children: [fndef],
+                    position: fndef.position
+                });
+            });
+
+            root.children.push({
+                type: 'thematicBreak',
+                position: children[0].position
+            });
+
+            root.children.push({
+                ordered: true,
+                type: 'list',
+                properties: {
+                    className: 'footnotes'
+                },
+                children: children,
+                position: children[0].position
+            });
+        }
+
+    };
+
+};
+
+/***/ }),
+/* 96 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var is = __webpack_require__(42)
+
+module.exports = remove
+
+function remove(ast, opts, test) {
+  var cascade
+
+  if (!test) {
+    test = opts
+    opts = {}
+  }
+
+  cascade = opts.cascade
+  cascade = cascade === null || cascade === undefined ? true : cascade
+
+  return preorder(ast, null, null)
+
+  // Check and remove nodes recursively in preorder.
+  // For each composite node, modify its children array in-place.
+  function preorder(node, nodeIndex, parent) {
+    var children
+    var length
+    var index
+    var position
+    var child
+
+    if (is(test, node, nodeIndex, parent)) {
+      return null
+    }
+
+    children = node.children
+
+    if (!children || children.length === 0) {
+      return node
+    }
+
+    // Move all living children to the beginning of the children array.
+    position = 0
+    length = children.length
+    index = -1
+
+    while (++index < length) {
+      child = preorder(children[index], index, node)
+
+      if (child) {
+        children[position++] = child
+      }
+    }
+
+    // Cascade delete.
+    if (cascade && position === 0) {
+      return null
+    }
+
+    // Drop other nodes.
+    children.length = position
+
+    return node
+  }
+}
+
 
 /***/ })
 /******/ ]);
